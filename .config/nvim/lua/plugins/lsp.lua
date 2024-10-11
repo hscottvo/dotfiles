c4d8755c21e5ea232693c0ee37ff98eca8109471
hscottvo <56327086+hscottvo@users.noreply.github.com>
2024-10-11 12:41:57 -0700

    Temporarily disable lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,122 +1,122 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
-		dependencies = {
-			-- main one
-			{ "ms-jpq/coq_nvim", branch = "coq" },
-
-			-- 9000+ Snippets
-			{ "ms-jpq/coq.artifacts", branch = "artifacts" },
-
-			-- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
-			-- Need to **configure separately**
-			{ "ms-jpq/coq.thirdparty", branch = "3p" },
-			-- - shell repl
-			-- - nvim lua api
-			-- - scientific calculator
-			-- - comment banner
-			-- - etc
-		},
-		init = function()
-			vim.g.coq_settings = {
-				auto_start = true, -- if you want to start COQ at startup
-				-- Your COQ settings here
-			}
-		end,
-		config = function()
-			-- Your LSP settings here
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-				"rust_analyzer",
-				"pyright",
-				"clangd",
-				"bashls",
-				"docker_compose_language_service",
-			},
-			handlers = {
-				function(server_name)
-					require("lspconfig")[server_name].setup({})
-				end,
-				rust_analyzer = function()
-					require("lspconfig").rust_analyzer.setup({
-						testExplorer = true,
-					})
-				end,
-			},
-		},
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		--config = function()jk
-		opts = {
-			ensure_installed = {
-				-- python
-				"black",
-				"isort",
-				"mypy",
-				-- lua
-				"stylua",
-				-- c++
-				"clang-format",
-				-- bash
-				"shfmt",
-				--hypr
-				"hyprls",
-				-- zshrc
-				"beautysh",
-			},
-		},
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			build = ":TSUpdate",
-		},
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"bash",
-					"c",
-					"css",
-					"dockerfile",
-					"go",
-					"hyprlang",
-					"lua",
-					"markdown",
-					"python",
-					"rust",
-					"sql",
-					"yaml",
-				},
-			})
-		end,
-	},
-	{
-		"stevearc/conform.nvim",
-		opts = {
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "isort", "black" },
-				cpp = { "clang-format" },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
-		},
-	},
+	-- {
+	-- 	"williamboman/mason.nvim",
+	-- 	config = function()
+	-- 		require("mason").setup()
+	-- 	end,
+	-- },
+	-- {
+	-- 	"neovim/nvim-lspconfig",
+	-- 	lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
+	-- 	dependencies = {
+	-- 		-- main one
+	-- 		{ "ms-jpq/coq_nvim", branch = "coq" },
+	--
+	-- 		-- 9000+ Snippets
+	-- 		{ "ms-jpq/coq.artifacts", branch = "artifacts" },
+	--
+	-- 		-- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+	-- 		-- Need to **configure separately**
+	-- 		{ "ms-jpq/coq.thirdparty", branch = "3p" },
+	-- 		-- - shell repl
+	-- 		-- - nvim lua api
+	-- 		-- - scientific calculator
+	-- 		-- - comment banner
+	-- 		-- - etc
+	-- 	},
+	-- 	init = function()
+	-- 		vim.g.coq_settings = {
+	-- 			auto_start = true, -- if you want to start COQ at startup
+	-- 			-- Your COQ settings here
+	-- 		}
+	-- 	end,
+	-- 	config = function()
+	-- 		-- Your LSP settings here
+	-- 	end,
+	-- },
+	-- {
+	-- 	"williamboman/mason-lspconfig.nvim",
+	-- 	opts = {
+	-- 		ensure_installed = {
+	-- 			"lua_ls",
+	-- 			"rust_analyzer",
+	-- 			"pyright",
+	-- 			"clangd",
+	-- 			"bashls",
+	-- 			"docker_compose_language_service",
+	-- 		},
+	-- 		handlers = {
+	-- 			function(server_name)
+	-- 				require("lspconfig")[server_name].setup({})
+	-- 			end,
+	-- 			rust_analyzer = function()
+	-- 				require("lspconfig").rust_analyzer.setup({
+	-- 					testExplorer = true,
+	-- 				})
+	-- 			end,
+	-- 		},
+	-- 	},
+	-- },
+	-- {
+	-- 	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	-- 	--config = function()jk
+	-- 	opts = {
+	-- 		ensure_installed = {
+	-- 			-- python
+	-- 			"black",
+	-- 			"isort",
+	-- 			"mypy",
+	-- 			-- lua
+	-- 			"stylua",
+	-- 			-- c++
+	-- 			"clang-format",
+	-- 			-- bash
+	-- 			"shfmt",
+	-- 			--hypr
+	-- 			"hyprls",
+	-- 			-- zshrc
+	-- 			"beautysh",
+	-- 		},
+	-- 	},
+	-- },
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	opts = {
+	-- 		build = ":TSUpdate",
+	-- 	},
+	-- 	config = function()
+	-- 		require("nvim-treesitter.configs").setup({
+	-- 			ensure_installed = {
+	-- 				"bash",
+	-- 				"c",
+	-- 				"css",
+	-- 				"dockerfile",
+	-- 				"go",
+	-- 				"hyprlang",
+	-- 				"lua",
+	-- 				"markdown",
+	-- 				"python",
+	-- 				"rust",
+	-- 				"sql",
+	-- 				"yaml",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
+	-- {
+	-- 	"stevearc/conform.nvim",
+	-- 	opts = {
+	-- 		formatters_by_ft = {
+	-- 			lua = { "stylua" },
+	-- 			python = { "isort", "black" },
+	-- 			cpp = { "clang-format" },
+	-- 		},
+	-- 		format_on_save = {
+	-- 			timeout_ms = 500,
+	-- 			lsp_format = "fallback",
+	-- 		},
+	-- 	},
+	-- },
 	-- old autocomplete, less pain in the butt to set up than COQ
 	-- { "hrsh7th/cmp-nvim-lsp" },
 	-- { "hrsh7th/nvim-cmp" },
