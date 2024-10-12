35e1b3e611744ad16489328f57975de947f1b3a9
hscottvo <56327086+hscottvo@users.noreply.github.com>
2024-10-12 12:40:47 -0700

    Change flake name

--- nix_darwin/darwin.nix
+++ nix_darwin/darwin.nix
@@ -41,11 +41,11 @@
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
+    darwinConfigurations.scott = nix-darwin.lib.darwinSystem {
       modules = [ configuration ];
     };
 
     # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."simple".pkgs;
+    darwinPackages = self.darwinConfigurations.scott.pkgs;
   };
 }
