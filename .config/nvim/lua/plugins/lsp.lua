ae9bac70bff19cc11571125febad25660eca5a65
hscottvo <56327086+hscottvo@users.noreply.github.com>
2024-11-12 13:58:14 -0800

    Clean up lsp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -27,20 +27,9 @@ return {
 		"neovim/nvim-lspconfig",
 		lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
 		dependencies = {
-			-- main one
 			{ "ms-jpq/coq_nvim", branch = "coq" },
-
-			-- 9000+ Snippets
 			{ "ms-jpq/coq.artifacts", branch = "artifacts" },
-
-			-- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
-			-- Need to **configure separately**
 			{ "ms-jpq/coq.thirdparty", branch = "3p" },
-			-- - shell repl
-			-- - nvim lua api
-			-- - scientific calculator
-			-- - comment banner
-			-- - etc
 		},
 		init = function()
 			vim.g.coq_settings = {
