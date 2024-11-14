3e5b12c3962d3153c08b92406c2cb06d112a2f4a
Scott <hscottvo@gmail.com>
2024-11-14 09:40:21 -0800

    Remove useless comments

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -7,17 +7,8 @@ in
 {
   home.username = "scott";
   home.homeDirectory = if isLinux then "/home/scott" else "/Users/scott";
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     git
     keychain
@@ -26,24 +17,9 @@ in
 
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
 
   programs.zsh = {
     enable = true;
-    # autosuggestion.enable = true;
     historySubstringSearch.enable = true;
     syntaxHighlighting.enable = true;
     shellAliases = {
@@ -78,9 +54,7 @@ in
     secureSocket = false;
     plugins = with pkgs.tmuxPlugins; [
       better-mouse-mode
-      # gruvbox
       nord
-      # mode-indicator
       resurrect
     ];
 
