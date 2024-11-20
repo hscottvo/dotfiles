f49bf407ec57830da43fff94338d87b35a19552b
hscottvo <56327086+hscottvo@users.noreply.github.com>
2024-11-20 11:38:05 -0800

    Add primeagen keybinds

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -5,15 +5,48 @@ local expr_opts = { silent = true, expr = true }
 
 local keymap = vim.keymap.set
 
-keymap({ "n" }, "<ESC>", ":noh<CR>:lua require('notify').dismiss()<CR>", opts)
+keymap("n", "<ESC>", ":noh<CR>:lua require('notify').dismiss()<CR>", opts)
+
+-- Move selection up and down, with indents
+keymap("v", "J", ":m '>+1<CR>gv=gv")
+keymap("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor in place when merging lines
+keymap("n", "J", "mzJ`z")
+
+-- Keep things in the middle
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
+keymap("n", "n", "nzzzv")
+keymap("n", "N", "Nzzzv")
+
+-- Pasting doesn't overwrite clipboard with what was written over
+keymap("x", "<leader>p", '"_dP')
+
+-- Disable Q
+keymap("n", "Q", "<nop")
+
+-- Scroll through quick fixes
+keymap("n", "<C-k>", "<cmd>cnext<CR>zz")
+keymap("n", "<C-j>", "<cmd>cprev<CR>zz")
 
 local harpoon = require("harpoon")
 
 local wk = require("which-key")
 
 wk.add({
+	-- Copy to clipboard
+	{ "<leader>y", '"+y', hidden = true },
+	{ "<leader>y", '"+y', hidden = true },
+	{ "<leader>Y", '"+Y', hidden = true },
+	-- vim options
+	{ "<leader>j", "<cmd>lprev<CR>zz", desc = "Scroll quickfix up" },
+	{ "<leader>k", "<cmd>lprev<CR>zz", desc = "Scroll quickfix down" },
 	-- Find
 	{ "<leader>f", desc = "Find" },
+
+	-- Replace current word
+	{ "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], desc = "Replace current word" },
 
 	-- lsp
 	{ "<leader>l", desc = "LSP" },
@@ -74,6 +107,4 @@ wk.add({
 		end,
 		desc = "Select buffer 4",
 	},
-
-	{ "<leader>h", group = "Hop" },
 })
