2b813bcebf2ce669b4fbb505c95ea4ded6fc75ff
Scott <hscottvo@gmail.com>
2025-03-09 12:31:58 -0700

    Add ghostty to mac shell config

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -12,15 +12,58 @@
       configuration = { pkgs, ... }: {
         # List packages installed in system profile. To search by name, run:
         # $ nix-env -qaP | grep wget
-        environment.systemPackages =
+        environment.systemPackages = with pkgs;
           [
-            pkgs.vim
-            pkgs.signal-desktop
+            vim
+            signal-desktop
           ];
+        homebrew = {
+          enable = true;
+          brews = [ "sdl3" ];
+          casks = [ "ghostty" "vlc" ];
+        };
 
         # Auto upgrade nix package and the daemon service.
-        services.nix-daemon.enable = true;
+        # services.nix-daemon.enable = true;
         # nix.package = pkgs.nix;
+        services.aerospace = {
+
+          enable = true;
+          settings = {
+            gaps = {
+              outer.left = 8;
+              outer.bottom = 8;
+              outer.top = 8;
+              outer.right = 8;
+            };
+            mode.main.binding = {
+              alt-h = "focus left";
+              alt-j = "focus down";
+              alt-k = "focus up";
+              alt-l = "focus right";
+
+              alt-1 = "workspace --auto-back-and-forth 1";
+              alt-2 = "workspace --auto-back-and-forth 2";
+              alt-3 = "workspace --auto-back-and-forth 3";
+              alt-4 = "workspace --auto-back-and-forth 4";
+              alt-5 = "workspace --auto-back-and-forth 5";
+              alt-6 = "workspace --auto-back-and-forth 6";
+              alt-7 = "workspace --auto-back-and-forth 7";
+              alt-8 = "workspace --auto-back-and-forth 8";
+              alt-9 = "workspace --auto-back-and-forth 9";
+
+              alt-shift-1 = "move-node-to-workspace 1";
+              alt-shift-2 = "move-node-to-workspace 2";
+              alt-shift-3 = "move-node-to-workspace 3";
+              alt-shift-4 = "move-node-to-workspace 4";
+              alt-shift-5 = "move-node-to-workspace 5";
+              alt-shift-6 = "move-node-to-workspace 6";
+              alt-shift-7 = "move-node-to-workspace 7";
+              alt-shift-8 = "move-node-to-workspace 8";
+              alt-shift-9 = "move-node-to-workspace 9";
+            };
+          };
+        };
 
         # Necessary for using flakes on this system.
         nix.settings.experimental-features = "nix-command flakes";
@@ -38,6 +81,7 @@
 
         # The platform the configuration will be used on.
         nixpkgs.hostPlatform = "aarch64-darwin";
+        nixpkgs.config.allowUnfree = true;
       };
     in
     {
