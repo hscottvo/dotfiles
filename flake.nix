4e4144a9fc2632cd176604ff9ba9e7fc13bdf8ec
Scott Vo <scott.vo@mechanical-orchard.com>
2026-03-25 14:05:16 -0700

    dev: add devShell with nix/lua tooling

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "dotfiles dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [ "aarch64-darwin" "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nixpkgs-fmt
+            stylua
+            lua-language-server
+            nil
+          ];
+        };
+      });
+    };
+}
