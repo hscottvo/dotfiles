3b4b1329640b85cd7514f8d1189d710ba7ec99c4
Scott Vo <scott.vo@mechanical-orchard.com>
2026-03-28 00:18:02 -0700

    Update librewolf config

--- home-manager/global/apps.nix
+++ home-manager/global/apps.nix
@@ -19,6 +19,14 @@
 
   programs.librewolf = {
     enable = true;
+    policies = {
+      ExtensionSettings = {
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
     profiles.main = {
       isDefault = true;
       settings = {
@@ -29,6 +37,9 @@
         "privacy.clearOnShutdown.cookies" = false;
         "privacy.clearOnShutdown.history" = false;
         "privacy.clearOnShutdown.sessions" = false;
+        "privacy.clearOnShutdown_v2.cookiesAndStorage" = false;
+        "privacy.clearOnShutdown_v2.historyFormDataAndDownloads" = false;
+        "privacy.sanitize.sanitizeOnShutdown" = false;
       };
     };
   };
