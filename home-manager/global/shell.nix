a2e8f88dba0cfa6a53b378918a552246e58898fe
Scott Vo <scott.vo@mechanical-orchard.com>
2026-04-03 10:28:45 -0700

    Clean up zsh config: remove oh-my-zsh, zsh-nix-shell, manual fzf source

--- home-manager/global/shell.nix
+++ home-manager/global/shell.nix
@@ -61,28 +61,8 @@ in
     shellAliases = {
       ll = "eza -l";
       ls = "eza";
-      neofetch = "fastfetch";
       dr = "sudo darwin-rebuild --impure switch --flake ~/.dotfiles/darwin#Scotts-Macbook-Pro";
     };
-    oh-my-zsh = {
-
-      enable = true;
-    };
-    plugins = [
-      {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.8.0";
-          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-        };
-      }
-    ];
-    envExtra = ''
-      source <(fzf --zsh)
-    '';
   };
 
   programs.tmux = {
@@ -299,6 +279,7 @@ in
 
   programs.fzf = {
     enable = true;
+    enableZshIntegration = true;
   };
 
 }
