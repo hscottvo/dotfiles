d9b6c227bc9e0a9aa67b20461292100418584b36
Scott Vo <scott.vo@mechanical-orchard.com>
2026-08-05 17:23:37 -0700

    fix: remove custom treesitter walking functions
    vim ships with  visual-mode `an` and `in`

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -29,48 +29,6 @@ local parsers = {
 	"yaml",
 }
 
-local function set_visual(s_row, s_col, e_row, e_col)
-	vim.api.nvim_win_set_cursor(0, { s_row + 1, s_col })
-	vim.cmd("normal! v")
-	vim.api.nvim_win_set_cursor(0, { e_row + 1, math.max(e_col - 1, 0) })
-end
-
-local function init_selection()
-	local node = vim.treesitter.get_node()
-	if not node then
-		return
-	end
-	vim.b.ts_sel_node = node
-	set_visual(node:range())
-end
-
-local function node_incremental()
-	local node = vim.b.ts_sel_node or vim.treesitter.get_node()
-	if not node then
-		return
-	end
-	local parent = node:parent()
-	if not parent then
-		set_visual(node:range())
-		return
-	end
-	vim.b.ts_sel_node = parent
-	set_visual(parent:range())
-end
-
-local function node_decremental()
-	local node = vim.b.ts_sel_node
-	if not node then
-		return
-	end
-	local child = node:child(0)
-	if not child then
-		return
-	end
-	vim.b.ts_sel_node = child
-	set_visual(child:range())
-end
-
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
@@ -93,10 +51,6 @@ return {
 					vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 				end,
 			})
-
-			vim.keymap.set("n", "<Enter>", init_selection, { desc = "Treesitter: init selection" })
-			vim.keymap.set("x", "<Enter>", node_incremental, { desc = "Treesitter: expand selection" })
-			vim.keymap.set("x", "<Backspace>", node_decremental, { desc = "Treesitter: shrink selection" })
 		end,
 	},
 	{ "nvim-treesitter/nvim-treesitter-context" },
